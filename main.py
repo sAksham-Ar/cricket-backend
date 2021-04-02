a2b38d67e659db7cdd1a480e93300b6a6604016a
Saksham Arya <aryasaksham@gmail.com>
2021-04-02 22:48:37 +0530

    created backend

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from criapi import Cricbuzz
+import json
+
+app=Flask(__name__)
+c=Cricbuzz()
+
+@app.route("/")
+def home():
+    return "The server is live!"
+
+@app.route("/matches")
+def matches():
+    return json.dumps(c.livescore())
+
+@app.route("/commentary/<id>")
+def commentary(id):
+    return json.dumps(c.commentary(id))
+
+
+@app.route("/scorecard/<id>")
+def scorecard(id):
+    return json.dumps(c.scorecard(id))
+
+
+if __name__ == "__main__":
+    app.run()
